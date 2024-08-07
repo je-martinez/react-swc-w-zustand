15fbeb1802563577935ad81d9a9b34d07293a35e
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-08-06 20:19:05 -0600

    chore: more setup

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import FeedItem from "../../components/feed-item";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <FeedItem />
+      <FeedItem />
+      <FeedItem />
+      <FeedItem />
+      <FeedItem />
+    </>
+  );
+};
+
+export default HomePage;
