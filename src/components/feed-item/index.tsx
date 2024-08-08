1e2a9a5221a5dd45b1b1cf3aed7d5ede4a42a82b
Jose E. Martinez <josemartinezchevez@gmail.com>
2024-08-08 14:57:45 -0600

    chore: shuffle array

--- src/components/feed-item/index.tsx
+++ src/components/feed-item/index.tsx
@@ -1,21 +1,34 @@
 import React from "react";
+import { Post, Comment, User } from "../../types";
 
-const FeedItem: React.FC = () => {
+interface FeedItemProps {
+  post: Post;
+  comments: Comment[];
+  user: User;
+}
+
+const FeedItem: React.FC<FeedItemProps> = ({
+  post,
+  comments,
+  user,
+}: FeedItemProps) => {
   return (
     <div className="bg-gray-50 dark:bg-black p-10 flex items-center justify-center">
       <div className="bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 p-4 rounded-xl border max-w-xl">
         <div className="flex justify-between">
           <div className="flex items-center">
-            <img
-              className="h-11 w-11 rounded-full"
-              src="https://pbs.twimg.com/profile_images/1287562748562309122/4RLk5A_U_x96.jpg"
-            />
+            {user?.username && (
+              <img
+                className="h-11 w-11 rounded-full"
+                src={`https://avatar.iran.liara.run/public?username=${user.username}`}
+              />
+            )}
             <div className="ml-1.5 text-sm leading-tight">
               <span className="text-black dark:text-white font-bold block ">
-                Visualize Value
+                {user?.name}
               </span>
               <span className="text-gray-500 dark:text-gray-400 font-normal block">
-                @visualizevalue
+                @{user?.username?.toLowerCase()}
               </span>
             </div>
           </div>
@@ -29,8 +42,7 @@ const FeedItem: React.FC = () => {
           </svg>
         </div>
         <p className="text-black dark:text-white block text-xl leading-snug mt-3">
-          “No one ever made a decision because of a number. They need a story.”
-          — Daniel Kahneman
+          {post.body}
         </p>
         <img
           className="mt-2 rounded-2xl border border-gray-100 dark:border-gray-700"
@@ -61,7 +73,9 @@ const FeedItem: React.FC = () => {
                 <path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path>
               </g>
             </svg>
-            <span className="ml-3">93 people are Tweeting about this</span>
+            <span className="ml-3">
+              {comments?.length} people are Tweeting about this
+            </span>
           </div>
         </div>
       </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import FeedItem from "../../components/feed-item";
+import useComments from "../../hooks/useComments";
+import usePosts from "../../hooks/usePosts";
+import useUsers from "../../hooks/useUsers";
+import { shuffle } from "../../utils";
 
 const HomePage: React.FC = () => {
+  const { getCommentsByPostId, getComments } = useComments();
+  const { posts, getPosts } = usePosts();
+  const { getUserById, getUsers } = useUsers();
+
+  useEffect(() => {
+    getComments();
+    getPosts();
+    getUsers();
+  }, [getComments, getPosts, getUsers]);
+
+  const postsRandom = useMemo(() => {
+    return shuffle(posts);
+  }, [posts]);
+
   return (
     <>
-      <FeedItem />
-      <FeedItem />
-      <FeedItem />
-      <FeedItem />
-      <FeedItem />
+      {postsRandom?.map((post) => (
+        <FeedItem
+          key={post.id}
+          post={post}
+          comments={getCommentsByPostId(post.id)}
+          user={getUserById(post.userId)!}
+        />
+      ))}
     </>
   );
 };
